125304db0d78a503d2e619529dc131ea393c8dea
fanhl <fanhonglin@klicen.com>
2018-05-16 14:43:59 +0800

    倒计时

--- app/src/main/java/com/fanhl/coroutines/ui/CountdownActivity.kt
+++ app/src/main/java/com/fanhl/coroutines/ui/CountdownActivity.kt
@@ -1,17 +1,21 @@
-package com.fanhl.coroutines
+package com.fanhl.coroutines.ui
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
-import kotlinx.android.synthetic.main.activity_main.*
+import com.fanhl.coroutines.R
+import kotlinx.android.synthetic.main.activity_countdown.*
 import kotlinx.coroutines.experimental.android.UI
 import kotlinx.coroutines.experimental.delay
 import kotlinx.coroutines.experimental.launch
 
-class MainActivity : AppCompatActivity() {
+/**
+ * 倒计时
+ */
+class CountdownActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_countdown)
 
         launch(UI) {
             for (i in 10 downTo 1) { // countdown from 10 to 1
